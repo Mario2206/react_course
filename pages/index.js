ca6c93f9e9cc6a962f31422848d866f3d3404711
Mathieu <mathieu.raimbault@hotmail.com>
2021-12-15 10:30:04 +0100

    :sparkles: add todolist

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from "react"
 
 export default function Home() {
+
+  const [count, setCount] = useState(0)
+  const [ todoList, setTodoList ] = useState(["value"])
+  const [currentTodoValue, setCurrentTodoValue] = useState("")
+  const [currentTodoIndex, setCurrentTodoIndex] = useState(null)
+  console.log({count});
+  function onChangeTodoInput(e) {
+    setCurrentTodoValue(e.target.value)
+  }
+
+  function addTodo() {
+    setTodoList(prev => [...prev, currentTodoValue])
+    setCurrentTodoValue("")
+  }
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +30,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>Hello !</h1>
+        <input value={currentTodoValue} onChange={onChangeTodoInput} />
+        <button onClick={addTodo}>Add todo</button>
+        {
+          todoList.length > 0 && (
+            todoList.map((todo, index) => (
+              <div key={index} style={{display: "flex"}}>
+                <input value={todo} disabled={currentTodoIndex !== index} onChange={(e) => {setTodoList(todoList.map((todo, i) => i === index ? e.target.value : todo  ))}}/>
+                <button onClick={() =>setTodoList(prev => prev.filter((_, i) => i !== index))}>Delete</button>
+                <button  onClick={()=>{console.log("test"); setCurrentTodoIndex(index)}}>Edit</button>
+              </div>
+              
+            ))
+          )
+        }
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
